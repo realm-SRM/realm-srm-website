3bde56b2da36849400d5fdf7e225a00351fd2d82
Priyanshu Vasudev <142427709+Priyanshu2608@users.noreply.github.com>
2025-01-16 21:46:22 +0530

    ourTeampage

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -4,14 +4,12 @@ import Mountain from '../assets/Group 23.png';
 const Team = () => {
   return (
     <div className="relative w-full h-screen bg-[#141930] flex justify-center items-center text-center">
-      {/* Background Mountain Image */}
+      
       <img
         src={Mountain}
         alt="Mountain Background"
         className="absolute bottom-0 left-0 w-full h-1/2 object-cover z-0"
       />
-
-      {/* Text Content */}
       <div className="z-10 mt-[-300px] text-[#FFDCC1]">
         <h1 className="text-[120px] font-bold tracking-wider">MEET OUR TEAM</h1>
         <p className="text-[54px] opacity-[70%] font-bold tracking-wide">slogan</p>
