9704d26766e86f2623395a4d434796a31d29599b
DevanshuTripathi120 <aditheprince120@gmail.com>
2025-01-25 01:03:35 +0530

    feat: Added content

--- src/Components/Accelerators.jsx
+++ src/Components/Accelerators.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
 const Accelerators = () => {
+
+  const members = {
+    "Acc 1": {  }
+  }
+
   return (
-    <div>
-      
+    <div className='bg-[#141930] pt-24 px-10 h-screen ' >
+      <div className="flex flex-row justify-between" >
+          <div className='Content' >
+
+            <p className='text-4xl text-[#FFDCC1] font-bold mb-4 ' >Accelerator</p>
+            <p className="text-[#FFFFFF] text-xl w-[25vw] " >
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis.
+            </p>
+
+          </div>
+
+          <div className='PhotoScroller w-[50vw] ' >
+
+          </div>
+
+          <div className='ProfileCard w-[15vw] ' >
+
+          </div>
+
+      </div>
     </div>
   )
 }
